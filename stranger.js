b65be35fb0889d8179f03070e549294216b82283
gushanlongchuan <53longchuan@sina.com>
2015-05-17 22:03:16 -0400

    modify the url as what have said by Alice

--- stranger.js
+++ stranger.js
@@ -9,13 +9,24 @@ var _ = require('underscore');
 var router = express.Router();
 var mongoose = require("mongoose");
 var Post = require('./module/post_table.js')
+var stormpathAPI = require('stormpath');
 
-router.get('/:strangername', function(req, res){
-	var name = req.params.strangername
-	Post.find({Username: name},function(err, results){
+var client;
+stormpathAPI.loadApiKey('apiKey-212N7J7X3ZLZ23YTFP7OL972B.properties', function apiKeyFileLoaded(err, apiKey) {
+	client = new stormpathAPI.Client({apiKey: apiKey});
+});
+
+router.get('/:stranger_id', function(req, res){
+	var Stranger_id = req.params.stranger_id;
+	var s_id;
+	client.getResource('https://api.stormpath.com/v1/accounts/'+Stranger_id, function(err, strangerData){
+		s_id = strangerData.href;
+		Post.find({User_id: s_id},function(err, results){
 		if (err) return err
-		else res.render('stranger', {results : results, name:name})
+		else res.render('stranger', {results : results})
+		});
 	});
+
 });
 
 module.exports = router;
--- user.js
+++ user.js
@@ -9,13 +9,21 @@ var _ = require('underscore');
 var router = express.Router();
 var mongoose   = require("mongoose");
 var Post = require('./module/post_table.js')
+var stormpathAPI = require('stormpath');
+var client;
+stormpathAPI.loadApiKey('apiKey-212N7J7X3ZLZ23YTFP7OL972B.properties', function apiKeyFileLoaded(err, apiKey) {
+	client = new stormpathAPI.Client({apiKey: apiKey});
+});
 
-router.get('/:username', function(req, res) {
-	var name = req.params.username;
-	console.log('this is username')
-	Post.find({Username: name},function(err, results){
-		if (err) return err
-		else res.render('user', {results : results})
+router.get('/:user_id', function(req, res) {
+	var User_id = req.params.user_id;
+	var name;
+	client.getResource('https://api.stormpath.com/v1/accounts/'+User_id, function(err, userData){
+		name = userData.username
+		Post.find({Username: name},function(err, results){
+			if (err) return err
+			else res.render('user', {results : result})
+		});
 	});
 });	
 
