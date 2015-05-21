18f9c07bc11db94d348ad9148b03b503b59dd52f
Bowen Wang <bowenwang1225@gmail.com>
2015-05-21 16:25:20 -0400

    Working Discover without front-edn polished

--- discover.js
+++ discover.js
@@ -26,16 +26,18 @@ function renderForm(req,res,locals){
 router.use(csurf({ sessionKey: 'stormpathSession' }));
 
 router.route('/')
-	.get(function(req,res,next){
+  .get(function(req,res,next){
       var userList = new Array();
       var path;
       var discList = new Array();
+      var friendList = new Array();
+      var showList = new Array();
       
       //var index = 0;
       
-  		req.app.get('stormpathApplication').getAccounts(function(err, accounts) {
-    		if (err) return next(err);
-    		
+      req.app.get('stormpathApplication').getAccounts(function(err, accounts) {
+        if (err) return next(err);
+        
         // Get all users from stormpath and list them
         var i = 0;
         var items = accounts.items;
@@ -49,7 +51,6 @@ router.route('/')
         }
         
         var j = 0;
-        var friendList = new Array();
         var count = 0;
         var tot = 0;
         var disc = 0;
@@ -58,10 +59,11 @@ router.route('/')
         for(j=0;j<i;j++){
            var oneURL = userList[j];
            var other_id = oneURL.split("/")[5];
-           Friend.find({User_id:U_id, Friend_id:other_id}, function(err, result){
-              if (result.length != 0){
-                friendList[count] = result.Friend_id;
-                console.log("other id inside is: "+nonfriendList[count]+"count is "+count);
+           Friend.find({User_id:U_id, Friend_id:other_id}, function(err, results){
+              if (results.length != 0){
+                friendList[count] = results[0].Friend_id;
+                console.log(friendList);
+                console.log("other id inside is: "+friendList[count]+" count is "+count);
                 count = count + 1;
               }
               tot = tot + 1;
@@ -69,13 +71,16 @@ router.route('/')
                 tot = 0;
                 for(k=0;k<i;k++){
                   var part_id = userList[k].split("/")[5];
-                  if (friendList.indexOf(part_id) < 0) {
+                  if ((friendList.indexOf(part_id) < 0)&&(req.user.href!=userList[k])) {
                     Post.findOne({ User_id: userList[k]}, function(err, result) {
                       if (err) 
                         return console.error(err);
                       if(result != null){
                         discList[disc] = result.Image_path;
-                        console.log(discList)
+                        showList[disc] = result._id;
+                        console.log(result._id);
+                        console.log(showList);
+                        console.log(discList);
                         disc = disc + 1;
                       }
                       tot = tot + 1;
@@ -83,11 +88,8 @@ router.route('/')
                       if((disc == 10)||(tot == i)){
                         renderForm(req,res,{
                           dis: true,
-                          user: userList,
+                          posts: showList,
                           images: discList
-                          // user2: userList[1],
-                          // user3: userList[2],
-                          // user4: userList[3],
                         });
                       }
                     });
@@ -98,26 +100,7 @@ router.route('/')
               }
            });
         }
-        //console.log(nonfriendList);
-
-        // Need to pick each user's latest posts
-        // Post.findOne({ User_id: userList[0]}, function(err, result) {
-        //   if (err) 
-        //     return console.error(err);
-        //   path = result.Image_path;
-        //   console.log(result);
-
-        //   renderForm(req,res,{
-        //     dis: true,
-        //     user1: userList[0],
-        //     user2: userList[1],
-        //     user3: userList[2],
-        //     user4: userList[3],
-        //     Image_path: path
-        //   });
-          
-        // });
-  		});
-	});
+      });
+  });
 
 module.exports = router;
